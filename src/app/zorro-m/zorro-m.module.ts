09125bd9bab187402f5c2c7639cb5d39363f3485
omar.barreto <Omar.Barreto@ilumno.com>
2021-04-19 20:01:01 -0500

    Init create problems

--- src/app/zorro-m/zorro-m.module.ts
+++ src/app/zorro-m/zorro-m.module.ts
@@ -5,7 +5,11 @@ import { IconsProviderModule } from './../icons-provider.module';
 import { NzLayoutModule } from 'ng-zorro-antd/layout';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 import { NzCardModule } from 'ng-zorro-antd/card';
-
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
 
 @NgModule({
   declarations: [],
@@ -14,13 +18,23 @@ import { NzCardModule } from 'ng-zorro-antd/card';
     NzLayoutModule,
     NzMenuModule,
     NzCardModule,
-    IconsProviderModule
+    IconsProviderModule,
+    NzTableModule,
+    NzDividerModule,
+    NzButtonModule,
+    NzModalModule,
+    NzDropDownModule
   ],
   exports:[
     NzLayoutModule,
     NzMenuModule,
     NzCardModule,
-    IconsProviderModule
+    IconsProviderModule,
+    NzTableModule,
+    NzDividerModule,
+    NzButtonModule,
+    NzModalModule,
+    NzDropDownModule
   ]
 })
 export class ZorroMModule { }
